02b5b10b37668c7eb8cca00665e3b8f4bb77485e
FabioPerasso <perasso@yahoo.com.br>
2022-06-30 13:34:12 -0300

    Modal codificado

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$('#meuModal').on('shown.bs.modal', function () {
+$('#meuInput').trigger('focus')
+})
